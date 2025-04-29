34d62937c608f62ff7c9f79326cf3d00d4a3440e
jestruvec <jhonnyestruve@gmail.com>
2025-04-29 09:52:24 -0600

    se agregan listeners para mover la camara en moviles

--- src/lib/helpers/eventHandlers.ts
+++ src/lib/helpers/eventHandlers.ts
@@ -7,6 +7,10 @@ export let mouseDeltaX = 0;
 export let mouseDeltaY = 0;
 let mouseMoveTimeout: ReturnType<typeof setTimeout> | null = null;
 
+let isTouching = false;
+let lastTouchX = 0;
+let lastTouchY = 0;
+
 const { canvasDOM } = getDOMElements();
 
 export const handleClick = () => {
@@ -42,4 +46,34 @@ export const handleResize = () => {
   camera.updateProjectionMatrix();
 
   renderer.setSize(width, height);
+};
+
+export const handleTouchStart = (e: TouchEvent) => {
+  if (e.touches.length === 1) {
+    isTouching = true;
+    lastTouchX = e.touches[0].clientX;
+    lastTouchY = e.touches[0].clientY;
+  }
+};
+
+export const handleTouchMove = (e: TouchEvent) => {
+  if (isTouching && e.touches.length === 1) {
+    const touch = e.touches[0];
+
+    mouseDeltaX = touch.clientX - lastTouchX;
+    mouseDeltaY = touch.clientY - lastTouchY;
+
+    lastTouchX = touch.clientX;
+    lastTouchY = touch.clientY;
+
+    if (mouseMoveTimeout) clearTimeout(mouseMoveTimeout);
+    mouseMoveTimeout = setTimeout(() => {
+      mouseDeltaX = 0;
+      mouseDeltaY = 0;
+    }, 50);
+  }
+};
+
+export const handleTouchEnd = () => {
+  isTouching = false;
 };
